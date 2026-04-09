69c0e86f541870fab32b5611b89d3d4e3ab7bac5
glovelidk <dzhajmg@gmail.com>
2026-04-09 17:41:47 +0300

    Add files via upload

---
+++ pw_01.sql/practical_work_01.sql
@@ -0,0 +1,14 @@
+-- Database: bi_sql_data_student
+
+-- DROP DATABASE IF EXISTS bi_sql_data_student;
+
+CREATE DATABASE bi_sql_data_student
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
